fe94ca2694de8e1d39393558bbaf4da03da2ec6a
cleverlysisme <tuananhdao2862@gmail.com>
2023-08-20 21:19:13 +0700

    fix: fix postgres sql command

--- setup_postgres_dijkstra.sql
+++ setup_postgres_dijkstra.sql
@@ -1,16 +1,12 @@
 alter table public.hanoi_route add column target integer;
 alter table public.hanoi_route add column source integer;
 
-ALTER TABLE public.hanoi_route ADD id integer;
-UPDATE public.hanoi_route
-SET id = gid;
+select pgr_createTopology('public.hanoi_route', 0.0001, 'geom', 'gid');
 
-select pgr_createTopology('public.hanoi_route', 0.0001, 'geom', 'id');
-
-SELECT seq, node, edge, cost, geom FROM pgr_dijkstra('SELECT id, source, target, 
+SELECT seq, node, edge, cost, geom FROM pgr_dijkstra('SELECT gid as id, source, target, 
 st_length(geom) as cost FROM public.hanoi_route', 1, 3000, false) as di
 JOIN public.hanoi_route pt
-ON di.edge = pt.id;
+ON di.edge = pt.gid;
 
 CREATE OR REPLACE FUNCTION pgr_fromAtoB(
 IN tbl varchar,
@@ -45,14 +41,14 @@ ORDER BY the_geom <-> ST_GeometryFromText(''POINT('
 target := rec.id;
 -- Shortest path query (TODO: limit extent by BBOX)
 seq := 0;
-sql := 'SELECT id, geom, name, cost, source, target,
+sql := 'SELECT gid, geom, name, cost, source, target,
 ST_Reverse(geom) AS flip_geom FROM ' ||
-'pgr_dijkstra(''SELECT id , source::int, target::int, '
+'pgr_dijkstra(''SELECT gid as id , source::int, target::int, '
 || 'st_length(geom) as cost FROM '
 || quote_ident(tbl) || ''', '
 || source || ', ' || target
 || ' , false), '
-|| quote_ident(tbl) || ' WHERE edge = id ORDER BY seq';
+|| quote_ident(tbl) || ' WHERE edge = gid ORDER BY seq';
 -- Remember start point
 point := source;
 FOR rec IN EXECUTE sql
@@ -71,7 +67,7 @@ ST_EndPoint(''' || rec.geom::text || ''') ) )'
 INTO heading;
 -- Return record
 seq := seq + 1;
-gid := rec.id;
+gid := rec.gid;
 name := rec.name;
 cost := rec.cost;
 geom := rec.geom;
@@ -85,6 +81,3 @@ LANGUAGE 'plpgsql' VOLATILE STRICT;
 SELECT (route.geom) FROM (
 SELECT geom FROM pgr_fromAtoB('hanoi_route', 1, 0, 3, 4
 ) ORDER BY seq) AS route;
-
-
-
